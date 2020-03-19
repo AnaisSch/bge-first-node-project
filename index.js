c42cc2171cf9d3bfd26d3c3825d3d2e9853cf981
AnaisSch <60514726+AnaisSch@users.noreply.github.com>
2020-03-19 16:56:19 +0100

    add app.get on index.js

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const app = express();
 app.listen(3000, () => {
     console.log("SERVER STARTED...");
 });
+/* app.get('/', (request, response) => {
+    console.log(request);
+    console.log(response);
+    console.log('access to / path');
+}); */
+app.get('/', (req, res) => {
+    console.log('access to / path');
+    res.send("hello world !");
+});
