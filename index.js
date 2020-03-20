49297f87eb5e5304850e650326638543e8b75429
AnaisSch <60514726+AnaisSch@users.noreply.github.com>
2020-03-20 10:27:00 +0100

    create new html file to remove html string on index.js

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
-const express = require('express');
+const express = require("express"); // require = va chercher du code et met le 'ici'
+const fs = require("fs"); // Appel les fichiers de son système
 const app = express();
 app.listen(3000, () => {
     console.log("SERVER STARTED...");
 });
-/* app.get('/', (request, response) => {
-    console.log(request);
-    console.log(response);
-    console.log('access to / path');
-}); */
-app.get('/', (req, res) => {
-    console.log('access to / path');
-    res.send(`<!DOCTYPE html>
-        < html >
-            <head>
-                <title>Project-2</title>
-            </head>
-            <body>
-                <h1>hello world!</h1>
-            </body>
-        </html >
-        `);
+
+app.get("/", (req, res) => {
+    console.log("access to / path");
+    const html = fs
+        .readFileSync("./src/index.html"); //Lire le fichier de manière synchrone + chemin du fichier
+    console.log(typeof html, html); // Transformer en objet qui se télécharge et ne retransmet pas le texte : on a un fichier  binaire au lieu d'une chaine de caractère
+    res.send(html);
 });
 app.get("/about", (req, res) => {
-    res.send("about");
+    res.send("about"); // Rajouter la chaine de caractère about et c'est donc la réponse
 });
