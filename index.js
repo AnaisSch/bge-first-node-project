b19ed21b74c96fa425e6f02939c4e22e0f0fa4a5
AnaisSch <60514726+AnaisSch@users.noreply.github.com>
2020-03-20 10:35:22 +0100

    change charset definition to make code compliant again

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ app.listen(3000, () => {
 app.get("/", (req, res) => {
     console.log("access to / path");
     const html = fs
-        .readFileSync("./src/index.html"); //Lire le fichier de manière synchrone + chemin du fichier
+        .readFileSync("./src/index.html") //Lire le fichier de manière synchrone + chemin du fichier
+        .toString('utf8'); // qu'on veut une chaine de caractère en encodage utf-8
     console.log(typeof html, html); // Transformer en objet qui se télécharge et ne retransmet pas le texte : on a un fichier  binaire au lieu d'une chaine de caractère
     res.send(html);
 });
